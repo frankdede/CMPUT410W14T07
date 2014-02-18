011e2e80ff0c04f9a91ab7039f49cb1af31d8a67
Yuwei <yuwei@Yuweis-MacBook-Pro.local>
2014-02-18 02:10:36 -0700

    Add databasedelper

---
+++ sys/model/databasehelper.py
@@ -0,0 +1,42 @@
+import mysql.connector
+#read name and password from information file
+class Databasehelper:
+    def readpassword(self):
+        try:
+            f = open('../../information','r')
+            list = f.read(256)
+            name = list.split(',')[0]
+            password = list.split(',')[1]
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        except ValueError:
+            print "Could not convert data to an integer."
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+        return name,password
+#create connection to database
+    def connect(self):
+        from mysql.connector import errorcode
+        name,password = self.readpassword()
+        try:
+            self.cnx = mysql.connector.connect(user=name, password=password,host='127.0.0.1',database='c410')
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exists")
+            else:
+                print(err)
+# get dabase connector
+    def getdabaseconnector(self):
+        return self.cnx
+# close current connection
+    def close(self):
+        self.cnx.close()
+# return cursor of database
+    def getcursor(self):
+        return self.cnx.cursor()
+if __name__ == "__main__":
+    databasehelper = Databasehelper()
+    databasehelper.connect()
