de3df62d6ae9fbcc529ccc757a189f3e762a7f95
dyf102 <dyf102@gmail.com>
2014-03-01 02:11:03 -0700

    Change some update API

--- sys/controller/posthelper.py
+++ sys/controller/posthelper.py
@@ -19,6 +19,7 @@ class PostHelper:
         print query
         cur.execute(query)
         dbhelper.commit()
+        return cur.rowcount>0
 # for the specific person permission, you need to add it to user_permission table
     def addpostpermission(self,dbhelper,pid,aid):
         if not isinstance(dbhelper,Databasehelper):
@@ -30,33 +31,53 @@ class PostHelper:
         print query
         cur.execute(query)
         dbhelper.commit()
+        return cur.rowcount>0
 #type argument should be one of ['pid','aid','time','message','title','permission']
 #Ussage: updatepost(databasehelper,pid,type="html", permmision="public") check keyword argument
-    def updatepost(self,dbhelper,pid,**kargs,user_id=''):
+    def updateMessage(self,dbhelper,pid,newcontent):
         if not isinstance(dbhelper,Databasehelper):
             raise NameError('invalid argument')
         if not dbhelper.isconnect():
             dbhelper.connect()
         cur = dbhelper.getcursor()
-        for type in kargs:
-            newcontent = kargs[type]
-            if type in ['pid','aid','time','message','title','permission']:
-                if type == 'type' and newcontent not in['html','txt','markdown']:
-                    return None
-                elif type == 'permission' and newcontent not in['me','user','friends','fof','fomh','public']:
-                    return None
-                elif type == 'permission' and newcontent == 'user' and user_id =='':
-                    return None
-                else:
-                    query = "UPDATE post SET %s='%s' WHERE pid='%s'"%(type,newcontent,pid)
-                    print query
-                    cur.execute(query)
-                    if type == 'permission' and newcontent !='user':
-                        query = "DELETE FROM user_permission WHERE pid='%s'"%(pid)
-                        cur.execute(query)
-                        dbhelper.commit()
-                    elif type == 'permission':
-                        addpostpermission(dbhelper,pid,user_id);
+        query = "UPDATE post SET message='%s' WHERE pid='%s'"%(newcontent,pid)
+        cur.execute(query)
+        dbhelper.comit()
+        return cur.rowcount>0
+    def updateTitle(self,dbhelper,pid,newtitle):
+        if not isinstance(dbhelper,Databasehelper):
+            raise NameError('invalid argument')
+        if not dbhelper.isconnect():
+            dbhelper.connect()
+        cur = dbhelper.getcursor()
+        query = "UPDATE post SET title='%s' WHERE pid='%s'"%(newtitle,pid)
+        cur.execute(query)
+        dbhelper.comit()
+        return cur.rowcount>0
+##time format should be like 2014-03-01 01:37:50, if time is not specified, the time is current time.
+    def updateTime(self,dbhelper,pid,time = ''):
+        if not isinstance(dbhelper,Databasehelper):
+            raise NameError('invalid argument')
+        if not dbhelper.isconnect():
+            dbhelper.connect()
+        cur = dbhelper.getcursor()
+        if time  == '':
+            query = "UPDATE post SET time=NULL WHERE pid='%s'"%(pid)
+        else:
+            query = "UPDATE post SET time='%s' WHERE pid='%s'"%(time,pid)
+        cur.execute(query)
+        dbhelper.comit()
+        return cur.rowcount>0
+    def updatePermission(self,dbhelper,pid,newpermission,user=''):
+        if not isinstance(dbhelper,Databasehelper):
+            raise NameError('invalid argument')
+        if not dbhelper.isconnect():
+            dbhelper.connect()
+        cur = dbhelper.getcursor()
+        query = "UPDATE post SET permission='%s' WHERE pid='%s'"%(newpermission,pid)
+        cur.execute(query)
+        if newpermission == 'user':
+            adduserpermission()
 # delete a post or server post by pid, aid,you need indicate in type argument
     def deletepost(self,dbhelper,type,key):
         if not isinstance(dbhelper,Databasehelper):
@@ -72,6 +93,7 @@ class PostHelper:
         print query
         cur.execute(query)
         dbhelper.commit()
+        return cur.rowcount>0
 # get list of post that the user by aid can browse
     def getpostlist(self,dbhelper,aid):
         if not isinstance(dbhelper,Databasehelper):
@@ -79,38 +101,42 @@ class PostHelper:
         if not dbhelper.isconnect():
             dbhelper.connect()
         cur = dbhelper.getcursor()
-        #get permission with public
+        #get the post if it is public
         query = "SELECT * FROM post WHERE permission='public'"
         cur.execute(query)
         re = []
         for fid in cur:
             re.append(fid)
         utility.addpath('../model')
-        #get permission with me
+        #get the post if aid is its author
         query = "SELECT * FROM post WHERE permission='me' and aid='%s'"%(aid)
         cur.execute(query)
         for fid in cur:
             re.append(fid)
-        #get permission with user
+        #get the post if aid is specifiied user
         query = "SELECT * FROM post WHERE permission = 'user' and pid IN (SELECT pid from user_permission WHERE aid='%s')"%(aid)
         cur.execute(query)
         for fid in cur:
             re.append(fid)
-        #get permission with friends
+        #get the post if aid is the author's friend
         import authorhelper
         author_name = AuthorHelper().getnamebyaid(dbhelper,aid)
         query = "SELECT * FROM post WHERE permission = 'friends' and aid IN  (SELECT aid from author WHERE author_name IN (SELECT name1 FROM circle WHERE name2 ='%s' ))"%(author_name)
         cur.execute(query)
         for fid in cur:
             re.append(fid)
-        #get permission with fof
+        #get the post if aid is the author's friends`s friend
         query = "SELECT * FROM post WHERE permission = 'fof' and aid IN  (SELECT aid from author WHERE author_name IN (SELECT name1 FROM circle WHERE name2 IN (SELECT name1 FROM circle WHERE name2 = '%s')))"%(author_name)
         cur.execute(query)
         print query
         for fid in cur:
             re.append(fid)
-        #get permission with friends
-        
+        #get the post if aid is in the same host as the permission's requirement
+        query = "SELECT * FROM post WHERE permission='fomh' AND aid IN (SELECT a1.aid FROM author a1,author a2 WHERE a1.sid = a2.sid AND a2.aid = '%s')"%(aid)
+        print query
+        cur.execute(query)
+        for fid in cur:
+            re.append(fid)
         return re
 if __name__ == '__main__':
     dbhelper = Databasehelper()
