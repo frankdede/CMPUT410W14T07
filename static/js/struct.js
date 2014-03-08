5d6804f5153b6edbf0f178426985f8e95aeef0f4
frankdede <guanqi@ualberta.ca>
2014-03-08 13:40:43 -0700

    Added message flow

--- static/js/struct.js
+++ static/js/struct.js
@@ -1,3 +1,5 @@
+var $ulPaddingTop;
+
 $.get("/author/123", function(data){
 	if(data){
 		$("#struct-content").html(data);
@@ -19,6 +21,53 @@ function installClickListener(){
 	});
 
 	$("#postSubmitBtn").click(function(){
-		alert("postSubmitBtn")
+		smoothAdd('postListView','hello');
+	});
+
+
+}
+
+function addPost($id, $text)
+{
+	var $element = $('#' + $id);
+
+	var $ulHeight = $el.height();
+
+	//hide the overflow 
+	$el.css({
+		height:   $ulHeight,
+		overflow: 'hidden'
+	});
+
+	var $ulPaddingTop    = parseInt($el.css('padding-top'));
+	var $ulPaddingBottom = parseInt($el.css('padding-bottom'));
+
+	$el.prepend('<li>' + $text + '</li>');
+
+	var $first = $('li:first', $el);
+	var $last  = $('li:last',  $el);
+
+	var $foh = $first.outerHeight();
+
+	var $heightDiff = $foh - $last.outerHeight();
+
+	var $oldMarginTop = $first.css('margin-top');
+
+	$first.css({
+		marginTop: 0 - $foh,
+		position:  'relative',
+		top:       0 - $ulPaddingTop
+	});
+
+	$last.css('position', 'relative');
+
+	$el.animate({ height: $h + $heightDiff }, 1500)
+
+	$first.animate({ top: 0 }, 250, function() {
+		$first.animate({ marginTop: $oldMarginTop });
 	});
 }
+
+//function setRefreshTimer(){
+//
+//}
