07bacae6d71b36ebbb8805d58346c0b04b0ca01e
Paul Anderson <coniewt@users.noreply.github.com>
2014-03-13 14:11:04 -0600

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "USEAGE:"
-echo "ssh user@localhost:41070"
+echo "ssh user@localhost -p 41070"
 echo "http://localhost:41078"
 
 ssh \
-  -L 41070:cs410.cs.ualberta.ca:41070 ohaton.cs.ualberta.ca \
-  -L 41078:cs410.cs.ualberta.ca:41078 ohaton.cs.ualberta.ca \
--l user
+  -L 41070:cs410.cs.ualberta.ca:41070 \
+  -L 41078:cs410.cs.ualberta.ca:41078 \
+ohaton.cs.ualberta.ca
