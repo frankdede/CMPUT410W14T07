48f1fe95420dc7dec89c07ab63380fd7ac8ae19d
frankdede <guanqi@ualberta.ca>
2014-03-15 20:10:51 -0600

    Refactored post.py and create_table.sql

--- sys/model/post.py
+++ sys/model/post.py
@@ -1,42 +1,61 @@
 import json
 class Post:
-    def __init__(self,pid,aid,dates,title,message,type,permission):
+    def __init__(self,pid,aid,date,title,content,type,permission):
         self.pid = pid
         self.aid = aid
-        self.dates = dates
+        self.date = date
         self.title=title
-        self.message=message
+        self.content=content
         self.type=type
         self.permission=permission
-    def getdates(self):
-        return self.dates
-    def getaid(self):
+
+    def getDate(self):
+        return self.date
+
+    def getAid(self):
         return self.aid
-    def gettitle(self):
+
+    def getTitle(self):
         return self.title
-    def getmessage(self):
-        return self.message
-    def gettype(self):
+
+    def getContent(self):
+        return self.content
+
+    def getType(self):
         return self.type
-    def getpermission(self):
+
+    def getPermission(self):
         return self.permission
-    def setaid(self,aid):
+
+    def setAid(self,aid):
         self.aid = aid
-    def settitle(self,title):
+
+    def setTitle(self,title):
         self.title = title
-    def setmessage(self,message):
-        self.message=message
-    def setpermission(self,permission):
-        self.permission=permission
-    def setdates(self,dates):
-        self.dates = dates
-    def getpid(self):
+
+    def setContent(self,message):
+        self.content = content
+
+    def setPermission(self,permission):
+        self.permission = permission
+
+    def setDate(self,dates):
+        self.date = date
+
+    def getPid(self):
         return self.pid
+
     def tojson(self):
-        return {"pid":self.pid,"aid":self.aid,"date":self.dates,"title":self.title,"message":self.message,"type":self.type,"permit":self.permission}
-    def __eq__(self,other):
-        return (self.aid==other.aid and self.dates==other.dates and self.title==other.title and self.message==other.message and self.type==other.type and self.permission==other.permission)
+        return {"pid":self.pid,"aid":self.aid,"date":self.date,"title":self.title,"content":self.content,"type":self.type,"permission":self.permission}
+
 #convert from object to json file
-    def jsontopost(jsonstring):
-        dic = json.loads(jsonstring)
-        return Post(dic["pid"],dic["aid"],dic["date"],dic["title"],dic["message"],dic["type"],dic["permit"])
+def parse(jsonstring):
+    dic = json.loads(jsonstring)
+    return Post(dic["pid"],dic["aid"],dic["date"],dic["title"],dic["content"],dic["type"],dic["permission"])
+
+def parseList(jsonstring):
+    result = []
+    dic = json.loads(jsonstring)
+    for post in dic:
+        result.append(Post(dic["pid"],dic["aid"],dic["date"],dic["title"],dic["content"],dic["type"],dic["permission"]))
+    return result
