4c50bafb1fbc079c0cd754877db87df8e432f7bb
frankdede <guanqi@ualberta.ca>
2014-03-22 21:37:43 -0600

    Updated tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 _username=""
 echo -e "USEAGE:"
 echo -e "ssh user@localhost -p 41070"
@@ -7,6 +8,9 @@ read _username
 echo "http://localhost:41078"
 
 ssh \
-  -L 41070:cs410.cs.ualberta.ca:41070 \
-  -L 41078:cs410.cs.ualberta.ca:41078 \
+  -fN -L 41070:cs410.cs.ualberta.ca:41070 \
+  -fN -L 41078:cs410.cs.ualberta.ca:41078 \
 $_username@ohaton.cs.ualberta.ca
+
+ssh user@localhost -p 41070
+killall ssh
