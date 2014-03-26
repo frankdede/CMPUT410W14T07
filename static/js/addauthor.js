6d647d91cffd987b8e56137a5f19dc5878c529df
dyf102 <dyf102@gmail.com>
2014-03-27 00:20:58 +0800

    partly complet add author UI

--- static/js/addauthor.js
+++ static/js/addauthor.js
@@ -1,10 +1,11 @@
 var author_id;
-var author_list = new Array();
+var recommended_author_list = new Array();
+var search_author_list = new Array();
 function get_recommended_author_list(aid){
     $("#add_author_table").empty();
     $.getJSON(aid+"/recommended_authorlist.json",function(data2){
       $.each(data2,function(i,field){
-        author_list[i] = field;
+        recommended_author_list[i] = field;
         $("#add_author_table").append("<div class='row' style='width:300px'id='addrow"+i+"'> \
       <div class='col-md-1'>"+(i+1)+"</div> \
       <div class='col-md-6'> \
@@ -22,25 +23,38 @@ function get_recommended_author_list(aid){
         //$('#message_menue').append("<li><a href='#'>"+name+"</a></li>");
   });
   }
-function get_all_auther_list(aid){
+function search_auther_list(aid,url){
   $("#add_all_author_table").empty();
-    $.getJSON(aid+"/authorlist.json",function(data2){
+    $.getJSON(aid+url,function(data2){
       $.each(data2,function(i,field){
-        author_list[i] = field;
+        search_author_list[i] = field;
         $("#add_all_author_table").append("<tr><td>"+(i+1)+"</td><td>"+field.name+"</td> \
           <td>"+field.nickname+"</td><td><input type='checkbox' name='add_author_cb' data='"+i+"'></td></tr>");
+        $("#search_model").modal();      
       });
-        //var name = items.requested_name+" want to be your friend";
-        //$('#message_menue').append("<li><a href='#'>"+name+"</a></li>");
   });
 }
 $(document).ready(function(){
   get_author_id();
-
+  $("#search_button").click(function(){
+    search_click();
+});
 });
 function search_click(){
-  $("#search_model").modal();
-  get_all_auther_list(author_id);
+  raw_input = $("#search_input").val();
+  console.log(raw_input);
+  if (raw_input.length>50) {
+      $("#search_form").removeClass("form-group");
+      $("#search_form").addClass("form-group has-error");
+      $("#search_input").val("");
+      $("#search_input").attr("placeholder","Too long");
+      return;
+  }
+  if (raw_input!='undefined'&& raw_input!="") {
+    search_auther_list(author_id,"/author/search?key="+encodeURIComponent(raw_input));
+  }else{
+    search_auther_list(author_id,"/authorlist.json");
+  }
 }
 function get_author_id(){
   $.get("/ajax/aid",function(data){
@@ -48,7 +62,7 @@ function get_author_id(){
     get_recommended_author_list(data);
   $('body').on('click', '#addfriendbt', function () {
     var pos = parseInt($(this).attr("data"));
-     $.get(data+"/author/request",{recipient:author_list[pos].aid},function(data2){
+     $.get(data+"/author/request",{recipient:recommended_author_list[pos].aid},function(data2){
       if (data2 == "OK"){
       //$("#addrow"+pos).slideUp();
         $("#addfriendbt[data|=\""+pos+"\"]").attr("disabled","disabled");
