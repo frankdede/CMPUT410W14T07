c771be557b36887ce6a3c70e2c1f88bc9b0710d4
Zhao <zhao@localhost.localdomain>
2014-03-29 23:24:31 -0700

    fixed the post

--- static/js/struct.js
+++ static/js/struct.js
@@ -230,3 +230,17 @@ function addPostToList($id, $html,$speed)
 		$first.animate({ marginTop: $oldMarginTop });
 	});
 }
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(100);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
