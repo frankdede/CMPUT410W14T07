32470c3b145385ac7862e2df02fa819e99afee27
frankdede <guanqi@ualberta.ca>
2014-04-07 19:08:29 -0600

    Update global posts

--- sys/controller/serverController.py
+++ sys/controller/serverController.py
@@ -1,18 +1,18 @@
 import json
 from mysql.connector.errors import Error
 from DatabaseAdapter import *
-
+from ServerHelper import *
 class ServerController:
 
     def __init__(self,dbAdapter):
-        self.serverController = ServerController(dbAdapter)
+        self.serverHelper = ServerHelper(dbAdapter)
     
-    def doesServerExists(self,url):
     '''check whether the server exists or not'''
+    def doesServerExists(self,url):
 
         return self.serverHelper.doesServerExists(url)
 
-    def addServer(self,name,url,local):
     ''' add new server'''
-
+    def addServer(self,name,url,local):
+    
         return self.serverHelper.addServer(name,url,local)
--- sys/controller/serverHelper.py
+++ sys/controller/serverHelper.py
@@ -42,4 +42,4 @@ class ServerHelper:
           print("****************************************")
           return None
 
-        return cur.fetchall() > 0
+        return cur.rowcount > 0
